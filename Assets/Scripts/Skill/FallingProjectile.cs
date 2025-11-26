bc0e73262926323a898a01a544fbd243bb67229f
oppa073-hub <oppa073@gmail.com>
2025-11-27 00:36:12 +0900

    [feat]FallingProjectile구현

---
+++ Assets/Scripts/Skill/FallingProjectile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class FallingProjectile : MonoBehaviour
+{
+    [SerializeField] private float fallSpeed = 5f;
+    [SerializeField] private float lifeTime = 3f;
+
+    private float timer;
+
+    public void Init(float speed)
+    {
+        fallSpeed = speed;
+        timer = 0f;
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+        timer += Time.deltaTime;
+        if (timer >= lifeTime)
+        {
+            PoolManager.Instance.Return(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var player = other.GetComponentInParent<PlayerStatePattern>();
+            if (player != null)
+            {
+                player.TakeDamage(1);
+            }
+            PoolManager.Instance.Return(gameObject);
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            PoolManager.Instance.Return(gameObject);
+        }
+
+    }
+}
+
