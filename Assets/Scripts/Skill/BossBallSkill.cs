800254e753900c35ba2edb7bedbbaf5cd5321b27
oppa073-hub <oppa073@gmail.com>
2025-11-26 16:01:59 +0900

    [feat] pool매니저 & 발사 담당 스킬로직 구현

---
+++ Assets/Scripts/Skill/BossBallSkill.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class BossBallSkill : SkillBase
+{
+    [SerializeField] private int poolIndex = 1;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float speed = 8f;
+
+    public override void Cast()
+    {
+        // 1) 풀에서 공 오브젝트 꺼내오기
+        GameObject ball = PoolManager.Instance.Get(poolIndex);
+        if (ball == null) return;
+
+        ball.transform.position = firePoint.position;
+        ball.transform.rotation = Quaternion.identity;
+
+        // 2) 프로젝트일 초기화
+        var proj = ball.GetComponent<BossBallProjectile>();
+        if (proj != null)
+        {
+            proj.Init(owner, speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Skill/PoolManager.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Data;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager Instance { get; private set; }
+    public GameObject[] prefabs;
+
+    List<GameObject>[] pools;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        pools = new List<GameObject>[prefabs.Length];
+
+        for(int index = 0; index < pools.Length; index++)
+        {
+            pools[index] = new List<GameObject>();
+        }
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+
+         foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+         if (select == null)
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+        return select;
+    }
+}
