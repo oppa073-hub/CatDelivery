ad0636bf27d7137fe5a64734ec68c8c3e8a456da
oppa073-hub <oppa073@gmail.com>
2025-11-27 00:35:42 +0900

    [feat] 플레이어 스킬베이스 수정

---
+++ Assets/Scripts/Skill/PlayerSkillBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class PlayerSkillBase : MonoBehaviour
+{
+    protected PlayerStatePattern owner;
+
+    public virtual void Init(PlayerStatePattern player)
+    {
+        owner = player;
+    }
+
+    public abstract void Cast();
+}
